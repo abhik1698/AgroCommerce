4e768585d8e02f5f9b51e73e3f9abf9cae74c32b
Abhishek K <abhik1698@gmail.com>
2020-05-07 00:51:39 +0530

    Content must be served over HTTPS

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,4 +1,4 @@
 const NODE_ENV = "prod";
 
 export const SERVER_URL =
-  NODE_ENV === "prod" ? "http://leact.herokuapp.com" : "http://localhost:5000";
+  NODE_ENV === "prod" ? "https://leact.herokuapp.com" : "http://localhost:5000";
